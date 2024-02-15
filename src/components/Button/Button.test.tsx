37a88a9e988d1d3ce3800dc1239d97872ea842b7
nurzhann173 <nurzhann173@gmail.com>
2024-02-15 19:34:07 +0600

    updated test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -7,20 +7,16 @@ describe("Button test", () => {
   it("should show button with the secondary variant", () => {
     const { container } = render(<Button variant="secondary">Click me</Button>);
 
-    // Get the button element from the rendered component
     const button = container.querySelector("button");
 
-    // Check if the button element has the expected className
     expect(button?.className).toBe("button secondary");
   });
 
   it("should show button with the primary variant", () => {
     const { container } = render(<Button variant="primary">Click me</Button>);
 
-    // Get the button element from the rendered component
     const button = container.querySelector("button");
 
-    // Check if the button element has the expected className
     expect(button?.className).toBe("button primary");
   });
 });
